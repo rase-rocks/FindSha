cb5f7f88d1dc59447e604e69744935e42023c2da
Robert Atherton <rob@rase.rocks>
2019-03-22 07:56:25 +0000

    Initial Commit

---
+++ FindSha/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FindSha
+//
+//  Created by Robert Atherton on 22/03/2019.
+//  Copyright © 2019 Robert Atherton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
